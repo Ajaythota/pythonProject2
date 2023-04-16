c53fecfbed3d13c6d6d5e6b7f12ed2d4a2999d21
Ajaythota <ajaykumarthota@yahoo.com>
2023-04-16 20:53:58 +0530

    htmlPython- restAPI

--- htmlPython.py
+++ htmlPython.py
@@ -1,14 +1,22 @@
 from flask import  Flask,render_template
+import pandas as pd
+import os
 
 app=Flask("Website")
+path=os.getcwd()+"\data\dictionary.csv"
+df=pd.read_csv(path)
+print(df)
+#defi = df[df['word'] == 'accumulator']
+#print(str(defi['definition']))
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<station>/<date>")
-def about(station,date):
-    temp=45
-    return {"station":station,"date":date,"temp":temp }
+@app.route("/api/v1/<word>")
+def api(word):
+    defi= df[df['word'] == word]
+    defini=str(defi['definition'])
+    return  { "word": word,"defintion":defini}
 
 app.run(debug=True)
